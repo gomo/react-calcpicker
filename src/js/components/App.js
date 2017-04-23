94adf29f860daca4c08f6ed283d2acb374daf5b8
Masamoto Miyata <miyata@sincere-co.com>
2017-04-23 19:46:42 +0900

    Implement opening and closing of the calculator window. Adjust the location of the calculator window.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,6 +2,7 @@ import {Component} from 'react'
 import numeral from 'numeral'
 import 'numeral/locales'
 import Calculator from './Calculator'
+import Portal from 'react-portal'
 
 export default class App extends Component
 {
@@ -9,7 +10,7 @@ export default class App extends Component
     super(props);
     numeral.locale(props.locale)
     this.state = {
-      openCalclator: false,
+      openCalculator: false,
       amount: props.initialAmount
     }
   }
@@ -20,23 +21,37 @@ export default class App extends Component
     }
   }
 
+  toggleCalculator(){
+    this.setState({openCalculator: !this.state.openCalculator});
+  }
+
   onClickAmount(e){
     e.preventDefault();
+    this.toggleCalculator()
     return false;
+  }
+
+  onCloseCalculator(){
+    //Set the openCalculator to false when clicking the overlay or closing with the ESC key.
+    if(this.state.openCalculator == true){
+      this.setState({openCalculator: false});
+    }
   }
 
   render(){
     return (
       <div className="react-currency-calculator">
-        <button className={this.props.className} onClick={(e) => this.onClickAmount(e)}>{numeral(this.state.amount).format(this.props.currencyFormat)}</button>
-        <Calculator
-          initialAmount={this.state.amount}
-        />
+        <button ref="button" className={this.props.className} onClick={(e) => this.onClickAmount(e)}>
+          {numeral(this.state.amount).format(this.props.currencyFormat)}
+        </button>
+        <Portal closeOnEsc closeOnOutsideClick isOpened={this.state.openCalculator} onClose={() => this.onCloseCalculator()}>
+          <Calculator
+            initialAmount={this.state.amount}
+            onClickClose={() => this.toggleCalculator()}
+            button={this.refs.button}
+          />
+        </Portal>
       </div>
     );
   }
-}
-
-App.propTypes = {
-
 }
--- src/js/components/Calculator.js
+++ src/js/components/Calculator.js
@@ -10,15 +10,34 @@ export default class Calculator extends Component
       amount: props.initialAmount,
       format: '0,0[.]0[000000000]',
       operator: ' ',
+      x: 0,
+      y: 0,
     }
+
+    window.onresize = () => this.adjustPosition();
+  }
+
+  adjustPosition(){
+    if(this.refs.calculator){
+      const buttonRect = this.props.button.getBoundingClientRect();
+      const parentRect = this.refs.calculator.parentElement.getBoundingClientRect();
+      this.setState({
+        x: buttonRect.left - parentRect.left,
+        y: buttonRect.bottom - parentRect.top,
+      });
+    }
+  }
+
+  componentDidMount(){
+    this.adjustPosition();
   }
 
   render(){
     const locale = numeral.localeData();
     return (
-      <div className="react-currency-calculator__calculator">
+      <div ref="calculator" className="react-currency-calculator__calculator" style={{transform: `translate(${this.state.x}px, ${this.state.y}px)`}}>
         <div className="react-currency-calculator__calculator-header">
-          <Button classType="close" display="×" onClick={() => this.close()} />
+          <Button classType="close" display="×" onClick={this.props.onClickClose} />
         </div>
         <div className="react-currency-calculator__calculator-display">
           <div className="react-currency-calculator__calculator-display-operator">{this.state.operator}</div>
